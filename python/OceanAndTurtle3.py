8e42d4d27633c103ed486d9307176a45cf096352
Paul Austin <Canoetime@gmail.com>
2020-01-23 09:35:09 -0600

    Create OceanAndTurtle3.py
    Addin a bit of behavior that happens all on its own.

---
+++ python/OceanAndTurtle3.py
@@ -0,0 +1,47 @@
+# A turtle in a big pond that knows your voice
+
+from turtle import Turtle, Screen
+
+print("Creating and Ocean")
+ocean = Screen()
+ocean.title("Lets find Nemo")
+
+print("Creating a turtle")
+bob = Turtle()
+
+print("Defining some commands")
+def keyLeft():
+    bob.left(45)
+    
+def keyRight():
+    bob.right(45)
+
+def keyUp():
+    bob.forward(20)
+
+def keyDown():
+    bob.backward(20)
+
+def keyHome():
+    bob.goto(0, 0)
+    
+def swim():
+    bob.forward(10)
+    ocean.ontimer(swim, 200)
+    
+ocean.onkey(keyLeft, "Left")
+ocean.onkey(keyRight, "Right")
+ocean.onkey(keyUp, "Up")
+ocean.onkey(keyDown, "Down")
+#ocean.onkey(keyHome, "h")
+ocean.onkey((lambda: bob.goto(0,0)), "h")
+
+print("Awake the ocean")
+ocean.listen()
+
+ocean.ontimer(swim, 200)
+
+print("Now Rest")
+ocean.mainloop()
+
+print("All is done")
