19bf6b52336630a45d6a6b5d15e9918d9b018709
Paul Austin <Canoetime@gmail.com>
2020-01-31 09:32:40 -0600

    Create Drift.py

---
+++ python/Drift.py
@@ -0,0 +1,13 @@
+# Make a box drift across the screen
+# 
+
+from turtle import Turtle, Screen
+import math
+
+print("Creating an Ocean")
+ocean = Screen()
+ocean.title("Let's find Splash")
+ocean.setup(500, 500)
+#ocean.tracer(0)
+
+# Yes, there is more work to be done here :) 
