88caff92b2bda9eef50ef8fe34b2a801751a7d04
Paul Austin <Canoetime@gmail.com>
2020-01-31 09:48:41 -0600

    Update Drift.py

--- python/WindyDay/Drift.py
+++ python/WindyDay/Drift.py
@@ -1,13 +1,46 @@
-# Make a box drift across the screen
+# A box blowing across the water
 # SDG
 
 from turtle import Turtle, Screen
-import math
 
 print("Creating an Ocean")
 ocean = Screen()
 ocean.title("Let's find Splash")
 ocean.setup(500, 500)
+
+print("Creating a drawing turtle")
+
+# Get the pen ready
+bob = Turtle()
+bob.color('plum')
+bob.width(3)
+
+# Make the pen fast.
+bob.hideturtle()
+bob.speed(0)
 #ocean.tracer(0)
 
-# Yes, there is more work to be done here :) 
+colors = ['red', 'blue', 'green', 'orange', 'purple', 'black', 'plum']
+
+#Where the paper is 
+bx = -1000
+by = -1000
+    
+def throwPaper(x, y):
+    global bx, by
+    bx = x
+    by = y
+
+def tictoc():
+    global bx, by
+    print('tt', bx, by)
+    if (bx > -1000):
+        bob.up()
+        bob.goto(bx, by)
+        bob.down()
+        bob.circle(20)
+    bx = bx + 10
+    ocean.ontimer(tictoc,200)   
+    
+ocean.onclick(throwPaper)
+ocean.ontimer(tictoc, 1000)
