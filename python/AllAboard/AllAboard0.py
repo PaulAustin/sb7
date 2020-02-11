a2c0901d89ac7ea14f98bc2dc0639f83b6d59277
PaulAustin <canoetime@gmail.com>
2020-02-11 11:31:45 -0600

    Changed shebang for python3
    Using the more portable from
    #!/usr/bin/env python3

--- python/AllAboard/AllAboard0.py
+++ python/AllAboard/AllAboard0.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 #
 # Making an argument, or rather taking arguments
 # taking as many as you want to make
@@ -6,5 +6,7 @@
 #import the 'system' object
 import sys
 
+print ("Which python is being run?" , sys.executable)
 print ("Vector of arguements List:", sys.argv)
 print ("Number of arguments passed:", len(sys.argv))
+
--- python/AllAboard/AllAboard2.py
+++ python/AllAboard/AllAboard2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 #
 # Making an argument, or rather taking arguments
 # taking as many as you want to make
